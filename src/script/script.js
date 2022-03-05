8ae3b7450ba206546d6f3ca918af5427646edba7
MILAD <omidmorshedzade@gmail.com>
2022-03-06 03:10:56 +0330

    updata responsive sidebar

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,18 @@
 const list_items = document.querySelectorAll('.first-item-list-link')
 const list_submenus = document.querySelectorAll('.submenu-list')
 
+// script header **********
+
+
+
+// script common header & sidebar
+
+
+
+
 // script sidebar***********
 
+//*************submenu in sidebar */
 list_items.forEach(element => {
     element.addEventListener('click', ()=>{
         removeClass()
