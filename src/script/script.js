2ecfe627012e0070882ca979603c12d214b8b8a3
MILAD <omidmorshedzade@gmail.com>
2022-03-10 12:28:19 +0330

    Remove useless files

--- src/script/script.js
+++ src/script/script.js
@@ -1,33 +1,41 @@
-const list_items = document.querySelectorAll('.first-item-list-link')
-const list_submenus = document.querySelectorAll('.submenu-list')
-const btn_open_sidebar = document.querySelector('.btn-open-sidebar')
+const listItems = document.querySelectorAll('.first-item-list-link')
+const listSubmenus = document.querySelectorAll('.submenu-list')
+const btnOpenSidebar = document.querySelector('.btn-open-sidebar')
 const sidebar = document.querySelector('.sidebar')
-const btn_up_down = document.querySelector('.btn-up-down')
+const btnToggle = document.querySelector('.btn-up-down')
+const arrowMenuSidebar = document.querySelectorAll('.arrow')
 
 
 
 // script sidebar***********
 
 //*************submenu in sidebar */
-list_items.forEach(element => {
+listItems.forEach(element => {
     element.addEventListener('click', ()=>{
         removeClass()
         element.nextElementSibling.classList.toggle('active')
+        toggleArrow()
     })
 });
 
-
+function toggleArrow(){
+    if(arrowMenuSidebar.forEach(element, ()=> element.classList = ('arrow-active'))){
+        element.classList.remove('arrow-active')
+    }else{
+        element.classList.add('arrow-active')
+    }
+}
 function removeClass(){
-    list_submenus.forEach(element => {
+    listSubmenus.forEach(element => {
         element.classList.remove('active')
     });
 }
 //************btn open sidebar */
 
-btn_open_sidebar.addEventListener('click', ()=>{
+btnOpenSidebar.addEventListener('click', ()=>{
     sidebar.classList.toggle('open-sidebar')
 })
 
-btn_up_down.addEventListener('click', ()=>{
+btnToggle.addEventListener('click', ()=>{
     sidebar.classList.toggle('show')
 })
