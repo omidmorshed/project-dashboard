3a9bbd8bf4a9a7baeda099f8f26185043fd695ac
MILAD <omidmorshedzade@gmail.com>
2022-03-10 21:53:39 +0330

    add transition to submenu

--- src/script/script.js
+++ src/script/script.js
@@ -3,39 +3,69 @@ const listSubmenus = document.querySelectorAll('.submenu-list')
 const btnOpenSidebar = document.querySelector('.btn-open-sidebar')
 const sidebar = document.querySelector('.sidebar')
 const btnToggle = document.querySelector('.btn-up-down')
-const arrowMenuSidebar = document.querySelectorAll('.arrow')
+const arrowMenuSidebar = document.querySelector('.first-item-list-link .arrow')
 
 
 
 // script sidebar***********
 
-//*************submenu in sidebar */
 listItems.forEach(element => {
-    element.addEventListener('click', ()=>{
-        removeClass()
-        element.nextElementSibling.classList.toggle('active')
-        toggleArrow()
-    })
+    element.addEventListener('click', (e)=>{
+        e.target.nextElementSibling.classList.toggle('active')
+        const arrow = element.childNodes[2];
+        arrow.classList.toggle('arrow-active')
+    })    
 });
 
-function toggleArrow(){
-    if(arrowMenuSidebar.forEach(element, ()=> element.classList = ('arrow-active'))){
-        element.classList.remove('arrow-active')
-    }else{
-        element.classList.add('arrow-active')
-    }
-}
-function removeClass(){
-    listSubmenus.forEach(element => {
-        element.classList.remove('active')
-    });
-}
-//************btn open sidebar */
 
-btnOpenSidebar.addEventListener('click', ()=>{
-    sidebar.classList.toggle('open-sidebar')
-})
 
-btnToggle.addEventListener('click', ()=>{
-    sidebar.classList.toggle('show')
-})
+
+// listItems.forEach(element => {
+//     element.addEventListener('click', (e)=>{
+//         e.target.nextElementSibling.classList.toggle('active')
+//         // arrowMenuSidebar.classList.toggle('arrow-active')
+//         const arrow = element.childNodes.classList = ('arrow')
+//         console.log(arrow);
+//         // element.childNodes['.arrow'].classList.toggle('arrow-active')
+//     })    
+// });
+
+//*************submenu in sidebar */
+// listItems.forEach(element => {
+//     element.addEventListener('click', ()=>{
+//         element.nextElementSibling.classList.toggle('active')
+//     })
+// });
+
+// function toggleArrow(){
+//     if(arrowMenuSidebar.forEach(element, ()=> element.classList = ('arrow-active'))){
+//         element.classList.remove('arrow-active')
+//     }else{
+//         element.classList.add('arrow-active')
+//     }
+// }
+// function removeClass(){
+//     listSubmenus.forEach(element => {
+//         element.classList.remove('active')
+//     });
+// }
+// //************btn open sidebar */
+
+// btnOpenSidebar.addEventListener('click', ()=>{
+//     sidebar.classList.toggle('open-sidebar')
+// })
+
+// btnToggle.addEventListener('click', ()=>{
+//     sidebar.classList.toggle('show')
+// })
+
+
+
+
+
+
+
+
+
+// const isOpen = e.target.nextElementSibling.classList.contains('active')
+//         console.log(isOpen);
